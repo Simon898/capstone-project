333828b56879f321b8c1ccf7c09e8e0c15db94b2
Simonh898 <simonh898@gmail.com>
2024-07-25 19:00:39 +0200

    final updates

--- src/components/layout/Footer.js
+++ src/components/layout/Footer.js
@@ -16,9 +16,9 @@ import logoWhiteImage from './assets/logo-white.png';
 import pages from '../../utils/pages';
 
 const contacts = [
-  { icon: faLocationDot, info: '678 Pisa Ave, Chicago, IL 60611', },
-  { icon: faPhone, info: '(312) 593-2744', },
-  { icon: faEnvelope, info: 'customer@littlelemon.com', },
+  { icon: faLocationDot, info: 'Little Lemon, Chicago', },
+  { icon: faPhone, info: '(332) 12345-432', },
+  { icon: faEnvelope, info: 'info@littlelemon.com', },
 ];
 
 const socials = [
